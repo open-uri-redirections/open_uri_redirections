615bcf5df6ec08ef6b2a018b8e34be004daa7da1
Felix C. Stegerman <flx@obfusk.net>
2014-11-26 20:25:34 +0100

    fix multiple redirections

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,5 +15,7 @@ $samples_dir = File.dirname(__FILE__) + '/samples'
 FakeWeb.register_uri(:get, "http://safe.com/",    :response => open("#{$samples_dir}/http_safe.response").read)
 FakeWeb.register_uri(:get, "https://safe.com/",   :response => open("#{$samples_dir}/https_safe.response").read)
 
+FakeWeb.register_uri(:get, "http://safe2.com/",   :response => open("#{$samples_dir}/http_safe2.response").read)
+
 FakeWeb.register_uri(:get, "https://unsafe.com/", :response => open("#{$samples_dir}/https_unsafe.response").read)
 FakeWeb.register_uri(:get, "http://unsafe.com/",  :response => open("#{$samples_dir}/http_unsafe.response").read)
